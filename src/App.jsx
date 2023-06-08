b23e74e259a6d327da3d2abff680f0041a31434e
Nicolas GARCIA <garcianicopro@gmail.com>
2023-06-08 23:23:05 +0200

    first commit first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import "./App.css"
+import Nav from "./Components/Nav"
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+    <Nav/>
+    <main className="main">
+      <h1>Acceuil</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sit consequuntur 
+        laudantium officia temporibus, fugiat, quod aspernatur non nostrum velit neque 
+        inventore est hic praesentium molestiae eligendi? Expedita, magni cum.
+        </p>
+    </main>
+  </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+import "./Nav.css"
+import { useState } from "react";
+
+const Nav = () => {
+
+  const [showLinks, setShowLinks] = useState(false)
+
+const handleShowLinks = () =>{
+  setShowLinks(!showLinks)
+}
+
+console.log(showLinks)
+
+  return(
+    <nav className='navbar' >
+      <div className="navbar_logo">Logo</div>
+      <ul className={"navbar_links"}>
+        <li className="navbar_item">
+          <a href="#" className="navbar_link">
+            Acceuil
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="#" className="navbar_link">
+            Portfolio
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="#" className="navbar_link">
+            Services
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="#" className="navbar_link">
+            A propos
+          </a>
+        </li>
+        <li className="navbar_item">
+          <a href="#" className="navbar_link">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <button className="navbar_burger" onClick={handleShowLinks}>
+        <span className="burger-bar"></span>
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
