fb73956cfa9e7fe782b7479e54d02cd6f0ad25d0
Mathieu-B <Mathieubilliet@gmail.com>
2023-06-09 09:01:08 +0200

    LinkCharacteresPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
-import AuthPage from "./screen/AuthPage/AuthPage";
-import Home from "./screen/Home/Home";
-import Authdata from "../src/data/AuthData.json";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import AuthPage from "./Screens/AuthPage/AuthPage";
+import Characters from "./Screens/Characters/Characters";
+import Authdata from "../src/data/AuthData.json";
 import "./App.css";
-import Characters from "./Screens/Characters";
 
 
 
@@ -15,12 +14,13 @@ function App() {
     <>
       <div className="App">
         {isLogged ? (
-          <Home setIsLogged={setIsLogged} />
+          <Characters setIsLogged={setIsLogged} />
         ) : (
           <AuthPage Authdata={Authdata} setIsLogged={setIsLogged} />
           )}
           </div>
       <Routes>
+        <Route path="./" element={<AuthPage />} />
         <Route path="/characters" element={<Characters />} />
       </Routes>
     </>
